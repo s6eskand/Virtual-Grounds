b1dccf031f95ea97632bb67dd771b1fe65c9c977
Brandon <brandongoh@ymail.com>
2021-01-16 13:27:53 -0500

    landing page update

---
+++ frontend/src/views/Landing.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import Navbar1 from '../components/Navbar1';
+import Background from '../components/Background';
+function Landing(){
+
+    return(
+        <div>
+            <Navbar1/>
+            <Background/>
+        </div>
+    )
+}
+
+export default Landing;
